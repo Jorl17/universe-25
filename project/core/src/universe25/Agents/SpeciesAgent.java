a9001daa312a8bf96b7ca8a34b2564d24e8f198a
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-08-09 18:08:17 +0100

    Make species string static

--- project/core/src/universe25/Agents/SpeciesAgent.java
+++ project/core/src/universe25/Agents/SpeciesAgent.java
@@ -6,18 +6,18 @@ import com.badlogic.gdx.graphics.Texture;
  * Created by jorl17 on 09/08/15.
  */
 public abstract class SpeciesAgent extends Agent {
-    private final String species;
+    private static String species;
     protected SpeciesAgent(Texture texture, boolean shouldDisposeTexture, float fov, float seeDistance, float speed, String species) {
         super(texture, shouldDisposeTexture, fov, seeDistance, speed);
-        this.species = species;
+        SpeciesAgent.species = species;
     }
 
     protected SpeciesAgent(Texture texture, boolean shouldDisposeTexture, float fov, float seeDistance, float speed, boolean debugDrawFov, boolean debugDrawCellsUnderFov, boolean debugDrawGoals, boolean debugDrawfacing, String species) {
         super(texture, shouldDisposeTexture, fov, seeDistance, speed, debugDrawFov, debugDrawCellsUnderFov, debugDrawGoals, debugDrawfacing);
-        this.species = species;
+        SpeciesAgent.species = species;
     }
 
-    public String getSpecies() {
+    public static String getSpecies() {
         return species;
     }
 }
