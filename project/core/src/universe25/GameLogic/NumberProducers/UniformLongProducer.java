94b4f5c47c64baa77d052a9d8c07d73d5a64bd4c
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-08-09 22:34:10 +0100

    Fix UniformLongProducer

--- project/core/src/universe25/GameLogic/NumberProducers/UniformLongProducer.java
+++ project/core/src/universe25/GameLogic/NumberProducers/UniformLongProducer.java
@@ -7,9 +7,16 @@ import java.util.Random;
  */
 public class UniformLongProducer implements NumberProducer<Long> {
     private static Random random = new Random();
-    private int intervalLinclusive, intervalHexclusive;
+    private long intervalLinclusive, intervalHexclusive;
+
+    public UniformLongProducer(long intervalLinclusive, long intervalHexclusive) {
+        this.intervalLinclusive = intervalLinclusive;
+        this.intervalHexclusive = intervalHexclusive;
+    }
+
     @Override
     public Long produce() {
+        //FIXME This sounds so ugly
         return ((long)(random.nextDouble()*(intervalHexclusive-intervalLinclusive)));
     }
 }
