eac8d413e9bc251a07e1c0fff30e0ae9dcc99db2
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-08-15 04:50:16 +0100

    Fix typos in last commit. I'm going to sleep...

--- project/core/src/universe25/Agents/States/SimplisticAntStates/GoToFood.java
+++ project/core/src/universe25/Agents/States/SimplisticAntStates/GoToFood.java
@@ -1,6 +1,8 @@
 package universe25.Agents.States.SimplisticAntStates;
 
+import sun.management.resources.agent;
 import universe25.Agents.SimplisticAnt.SimplisticAnt;
+import universe25.Agents.States.AvoidCells;
 import universe25.Agents.States.GoToCells;
 import universe25.Agents.ValuePositionPair;
 import universe25.Food.FoodQuantityPair;
@@ -16,12 +18,12 @@ public class GoToFood extends GoToCells<SimplisticAnt> {
     }
 
     @Override
-    protected boolean areThereCellsToAvoid() {
+    protected boolean areThereCellsToGoTo() {
         return agent.areThereCellsWithFood();
     }
 
     @Override
-    protected ArrayList<ValuePositionPair<Float>> getCenterOfCellsToAvoid() {
+    protected ArrayList<ValuePositionPair<Float>> getCenterOfCellsToGoTo() {
         ArrayList<ValuePositionPair<FoodQuantityPair>> centerOfCellsInFieldOfViewWithFood = agent.getCenterOfCellsInFieldOfViewWithFood();
 
         // Need to convert the array above into an array with just the densities (floats)
