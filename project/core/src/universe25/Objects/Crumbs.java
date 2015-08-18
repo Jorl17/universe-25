f2e829bb85efc4705610391ab4f0a3844ea22f48
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-08-18 03:58:59 +0100

    Make Crumbs randomly rotated

--- project/core/src/universe25/Objects/Crumbs.java
+++ project/core/src/universe25/Objects/Crumbs.java
@@ -3,6 +3,7 @@ package universe25.Objects;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.Align;
 import universe25.Agents.Stackable.Food.Food;
+import universe25.Utils.RandomUtils;
 
 /**
  * Created by jorl17 on 14/08/15.
@@ -14,6 +15,7 @@ public class Crumbs extends  WorldObject {
         super(new Texture("crumbs.gif"));
         setSize(4,4);
         setOrigin(Align.center);
+        setRotation(RandomUtils.rand(0, 360.0f));
         this.source = source;
         this.foodAmount = foodAmount;
     }
