18a28cd3af6456625b40a214e7ad1e5daab2195f
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-08-19 22:39:06 +0100

    Add a GoTOCellIfVisible state. It accepts a Supplier of cells. Optionally, it can wrap a single cell in a constant cell supplier for fixed state cells

---
+++ project/core/src/universe25/Agents/States/GoToCellIfVisible.java
@@ -0,0 +1,48 @@
+package universe25.Agents.States;
+
+import universe25.Agents.Agent;
+import universe25.Agents.ValuePositionPair;
+import universe25.GameLogic.Movement.Pathfinding.GridCell;
+
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
+/**
+ * Created by jorl17 on 19/08/15.
+ */
+public class GoToCellIfVisible<T extends Agent> extends GoToCells<T> {
+    private ArrayList<ValuePositionPair<Float>> cellCentreAndValue;
+    private Supplier<GridCell> gridCellSupplier;
+    public GoToCellIfVisible(T agent, int priority, String name, Supplier<GridCell> gridCellSupplier) {
+        super(agent, priority, name);
+        this.gridCellSupplier = gridCellSupplier;
+        this.cellCentreAndValue = new ArrayList<>();
+    }
+
+    public GoToCellIfVisible(T agent, int priority, String name, GridCell cell) {
+        super(agent, priority, name);
+        this.gridCellSupplier = () -> cell;
+        this.cellCentreAndValue = new ArrayList<>();
+    }
+
+    @Override
+    protected boolean areThereCellsToGoTo() {
+        GridCell cell = gridCellSupplier.get();
+        for ( GridCell c : agent.getCellsInFov() )
+            if ( c.equals(cell) )
+                return true;
+
+        //System.out.println(cell + ", " + agent.getCellsInFov());
+        return false;
+    }
+
+    @Override
+    protected ArrayList<ValuePositionPair<Float>> getCenterOfCellsToGoTo() {
+        GridCell cell = gridCellSupplier.get();
+        this.cellCentreAndValue.clear();
+        this.cellCentreAndValue.add(new ValuePositionPair<>(1.0f, cell.getCentre()));
+        return cellCentreAndValue;
+    }
+
+
+}
