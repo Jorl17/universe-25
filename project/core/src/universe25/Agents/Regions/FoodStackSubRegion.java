0a6249e9e1c6e5b20d2c1f2836bd5305aa071e24
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-08-19 22:36:26 +0100

    Implement a (currently hacky and testing only) getCurrentFoodStackCell method to GoodStackSubRegion

--- project/core/src/universe25/Agents/Regions/FoodStackSubRegion.java
+++ project/core/src/universe25/Agents/Regions/FoodStackSubRegion.java
@@ -1,10 +1,17 @@
 package universe25.Agents.Regions;
 
+import universe25.GameLogic.Movement.Pathfinding.GridCell;
+
 /**
  * Created by jorl17 on 17/08/15.
  */
 public class FoodStackSubRegion extends SubRegion {
     public FoodStackSubRegion(Region parentRegion) {
         super(parentRegion);
+    }
+
+    public GridCell getCurrentFoodStackCell() {
+        //FIXME: Testing only, completely hacked in
+        return getCells().get(0);
     }
 }
