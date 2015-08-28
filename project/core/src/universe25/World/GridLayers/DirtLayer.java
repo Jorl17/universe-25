785fd636ce039e0820beb03e2a47b9186673733e
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-08-28 04:23:44 +0100

    Implement an initial spreadDIrtAtCell
    While ants dig, they will spread a given percentage of dirt to
    neighboring cells. This will come from a source (i.e. spreadSource)
    cell. The ideal way to do this would be to actually keep track of which
    cells have agents and deny throwing dirt at that. I’ll probably code
    that in the future.

--- project/core/src/universe25/World/GridLayers/DirtLayer.java
+++ project/core/src/universe25/World/GridLayers/DirtLayer.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.graphics.Color;
 import universe25.GameLogic.Movement.Pathfinding.GridCell;
 import universe25.Objects.Dirt;
 
+import java.util.ArrayList;
+
 /**
  * Created by jorl17 on 25/08/15.
  */
@@ -38,6 +40,28 @@ public class DirtLayer extends FloatLayer {
             setValueAtCell(col, row, 0.0f);
             return new Dirt(val);
         }
+    }
+
+    public void spreadDirtAtCell(GridCell cell, GridCell spreadSource, float percentToSpread) {
+        float valToSpread = getValueAtCell(cell) * percentToSpread;
+
+        ArrayList<GridCell> cells = new ArrayList<>();
+        GridCell tmp;
+        for ( int i = -1; i <= 1; i++ )
+            for ( int j = -1; j <= 1; j++ )
+                if ( ((tmp= getCellAt(cell.getCol() + i, cell.getRow() + j)) != null)
+                        && !tmp.equals(spreadSource)
+                        && !tmp.equals(cell) )
+                    cells.add(tmp);
+
+        if ( cells.isEmpty() ) return;
+
+        float amountPerNeighbourCell = valToSpread / cells.size();
+
+        for ( GridCell c : cells )
+            increaseValueAtCell(c, amountPerNeighbourCell);
+        
+        decreaseValueAtCell(cell, valToSpread);
     }
 
     public Dirt takeDirtFromCellAt(GridCell cell, float maxToTake) {
